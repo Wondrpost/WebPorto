602768f9558a816158e1b8145af9786284ea152e
Wondrpost <aaronikolast@gmail.com>
2025-06-11 22:37:47 +0700

    updated 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -53,11 +53,24 @@ const tabs = document.querySelectorAll('[data-target]'),
 
 
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
-
+let mixerPortofolio = mixitup('.work__container', {
+    selectors: {
+        target: '.work__card'
+    },
+    animation: {
+        duration: 300
+    }
+});
 
 /*===== Link Active Work =====*/
+const linkWork = document.querySelectorAll('.work__item')
 
+function activeWork(){
+    linkWork.forEach(L=> l.classList.remove('active-work'))
+    this.classList.add('active-work')
+}
 
+linkWork.forEach(L=> l.addEventListener("click",activeWork))
 /*===== Work Popup =====*/
 
 
